a53f0cd91ab4056dd3dc6dc4d991e091bc76a9c8
ved603 <gujjuvedant603@gmail.com>
2024-05-07 11:42:53 +0530

    Initial Coomit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,29 @@
+package org.example;
+//
+//import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+//        Scanner sc = new Scanner(System.in);
+//        System.out.print("Enter your First Name : ");
+//        String fn = sc.nextLine();
+//        System.out.print("Enter your Last Name : ");
+//        String ln = sc.nextLine();
+//        System.out.print("Enter your address : ");
+//        String adr = sc.nextLine();
+//        System.out.print("Enter your city : ");
+//        String ci = sc.nextLine();
+//        System.out.print("Enter your state : ");
+//        String st = sc.nextLine();
+//        System.out.print("Enter your Zip Code : ");
+//        String zip = sc.nextLine();
+//        System.out.print("Enter your Phone number : ");
+//        String phoneno = sc.nextLine();
+//        System.out.print("Enter your email : ");
+//        String email = sc.nextLine();
+//        Contact con = new Contact(fn,ln,adr,ci,st,zip,phoneno,email);
+//        System.out.println(con);
+        System.out.println("Hello World");
+    }
+}
